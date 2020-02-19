18c8eaef0a146f445a14ac075a8477089d5344f9
王立磊 <3232315217@qq.com>
2020-02-19 14:10:08 +0800

    3、查询数据库成功

--- src/test/main.java
+++ src/test/main.java
@@ -5,6 +5,7 @@ import com.buy.utils.DataSourceUtil;
 import javax.xml.crypto.Data;
 
 public class main{
+    //回家吧
     public static void main(String[] args) {
         DataSourceUtil.getConn();
     }
